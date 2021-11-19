f1ee0b3ba9ceac2c28356df8580f8126ba9b8be0
jvdHFZbm <jvdhfzbm@email.com>
2021-11-19 20:39:53 +0000

    Update Employee.java

--- src/main/java/com/epam/test/automation/java/practice6/Employee.java
+++ src/main/java/com/epam/test/automation/java/practice6/Employee.java
@@ -1,9 +1,33 @@
 package com.epam.test.automation.java.practice6;
+import java.math.BigDecimal;
 
-/**
- * <summary>
- * Implement code according to description of task.
- * </summary>
- */
-public class Employee {
+public abstract class Employee {
+    private String name;
+    private BigDecimal salary;
+   protected BigDecimal bonus;
+
+    public String name() {
+        return name;
+    }
+
+
+    public BigDecimal salary(BigDecimal salary) {
+        this.salary = salary;
+        return salary;
+    }
+
+    public Employee(String name, BigDecimal salary){
+        this.name=name;
+        this.salary=salary;
+    }
+
+    public abstract BigDecimal setBonus(BigDecimal bonus);
+
+    public BigDecimal getBonus() {
+        return bonus;
+    }
+
+    public BigDecimal toPay(){
+        return salary.add(bonus);
+    }
 }

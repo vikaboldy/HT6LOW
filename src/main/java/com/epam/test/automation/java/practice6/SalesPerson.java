facdeccd96c125227fc64d250837a747f1883969
jvdHFZbm <jvdhfzbm@email.com>
2021-11-19 20:41:13 +0000

    Update SalesPerson.java

--- src/main/java/com/epam/test/automation/java/practice6/SalesPerson.java
+++ src/main/java/com/epam/test/automation/java/practice6/SalesPerson.java
@@ -1,9 +1,25 @@
 package com.epam.test.automation.java.practice6;
 
-/**
- * <summary>
- * Implement code according to description of task.
- * </summary>
- */
-public class SalesPerson {
+import java.math.BigDecimal;
+
+
+public abstract class SalesPerson extends Employee {
+
+    private int percent;
+
+    public SalesPerson(String name, BigDecimal salary, int percent) {
+        super(name, salary);
+        this.percent=percent;
+    }
+
+
+    public BigDecimal setBonus(BigDecimal bonus) {
+
+        if(percent>100){
+            this.bonus=bonus.multiply(BigDecimal.valueOf(2));
+        } else if(percent>200){
+            this.bonus=bonus.multiply(BigDecimal.valueOf(3));
+        }
+        return bonus;
+    }
 }
